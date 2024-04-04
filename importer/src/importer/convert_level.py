ff99ed1edcfb21c13e4b0327ec3557f9d4e30d33
Stefan Grinsted <stefan@e-nation.dk>
2024-04-04 16:06:41 +0200

    Harvester and Farmer support, plus optimisations
    Primarily reformatted code, aside from added supoort

--- importer/src/importer/convert_level.py
+++ importer/src/importer/convert_level.py
@@ -3,53 +3,53 @@ import json
 import argparse
 import os
 
-def create_level_json(levelfiles:list, target_dir):
-  if isinstance(levelfiles, str):
-      levelfiles = [levelfiles]
 
-  loaded_json = []
+def create_level_json(levelfiles: list, target_dir):
+    if isinstance(levelfiles, str):
+        levelfiles = [levelfiles]
 
-  for levelfile in levelfiles:
-    try:
-      print("creating level for", levelfile)
-      root = ET.parse(levelfile)
+    loaded_json = []
 
-      # Find the 'config' node and extract the JSON data
-      config_node = root.find('config')
-      json_data = config_node.text
+    for levelfile in levelfiles:
+        try:
+            print("creating level for", levelfile)
+            root = ET.parse(levelfile)
 
-      # Load the JSON data as a Python dictionary
-      data = json.loads(json_data)
-      loaded_json.append(data)
+            # Find the 'config' node and extract the JSON data
+            config_node = root.find('config')
+            json_data = config_node.text
 
-      filename = os.path.basename(levelfile)
-      filename = os.path.splitext(filename)[0]
-      filename = f"{target_dir}/{filename}.json"
+            # Load the JSON data as a Python dictionary
+            data = json.loads(json_data)
+            loaded_json.append(data)
 
-      # Save the Python dictionary as a JSON file
-      with open(filename, 'w') as f:
-        json.dump(data, f, indent=2)
+            filename = os.path.basename(levelfile)
+            filename = os.path.splitext(filename)[0]
+            filename = f"{target_dir}/{filename}.json"
 
-      print(f"Saved level to '{filename}'")
-    except (AttributeError,KeyError) as err:
-      print(f"Error handling file: {levelfile} : ", err)
+            # Save the Python dictionary as a JSON file
+            with open(filename, 'w') as f:
+                json.dump(data, f, indent=2)
 
-  return loaded_json
+            print(f"Saved level to '{filename}'")
+        except (AttributeError, KeyError) as err:
+            print(f"Error handling file: {levelfile} : ", err)
+
+    return loaded_json
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(
-              prog='Level importer/converter',
-              description='Import a level file from code-dot-org and store it locally to the',
-              epilog='Text at the bottom of help')
+    parser = argparse.ArgumentParser(
+        prog='Level importer/converter',
+        description='Import a level file from code-dot-org and store it locally to the',
+        epilog='Text at the bottom of help')
 
+    parser.add_argument('levelfile', nargs='+',
+                        help='The file of the level to import and convert')
 
-  parser.add_argument('levelfile', nargs='+',
-              help='The file of the level to import and convert')
+    parser.add_argument('-t', '--target_dir', default="levels/", required=False,
+                        help='Target directory to store the converted files')
 
-  parser.add_argument('-t', '--target_dir', default="levels/", required=False,
-              help='Target directory to store the converted files')
+    args = parser.parse_args()
 
-  args = parser.parse_args()
-
-  create_level_json(args.levelfile,args.target_dir)
+    create_level_json(args.levelfile, args.target_dir)
