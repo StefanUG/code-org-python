4e0c32ba5f3a7f65084b2335c6856cd9827163fc
Stefan Grinsted <stefan@e-nation.dk>
2025-08-21 08:20:36 +0200

    Changes to test_all

--- test_all.py
+++ test_all.py
@@ -1,22 +1,23 @@
 import subprocess
 import os
 
-def run_script(script_path):
+def run_script(dir, file):
     env = os.environ.copy()
     # Add your custom environment variable
     env["TESTMODE"] = "True"
 
-    result = subprocess.run(['python', script_path], env=env, stdout=subprocess.PIPE)
+    result = subprocess.run(['python', file], env=env, stdout=subprocess.PIPE, cwd=dir)
     output = result.stdout.decode('utf-8').strip()
     
-    print(f"{output} {script_path}")
+    print(f"{output} {os.path.join(dir, file)}")
 
 def visit_dir(dir):
     for file in os.listdir(dir):
-        if os.path.isdir(file):
-            visit_dir(os.path.join(dir, file))
-        elif file.endswith(".py") and file != "test_all.py":
-            run_script(os.path.join(dir, file))
+        fullpath = os.path.join(dir,file)
+        if os.path.isdir(fullpath):
+            visit_dir(fullpath)
+        elif file.endswith(".py") and file.startswith("test_") and file != "test_all.py":
+            run_script(dir, file)
 
 
 if __name__ == "__main__":
