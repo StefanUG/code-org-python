787a9bf4fc6277a507f1e5af194c36cdb8f61249
Stefan Grinsted <stefan@e-nation.dk>
2025-09-23 17:58:06 +0200

    Added levels from Course F 2017

---
+++ coursef-2017/8 - Ramp Up to Course F (Optional) - Nested Loops in Bee-Zombie/4_maze_predict.py
@@ -0,0 +1,35 @@
+from maze import Puzzle, BeePlayer
+
+maze = Puzzle.from_file("courseD_bee_nestedLoops2_predict1")
+bee: BeePlayer = maze.player
+
+'''
+https://studio.code.org/s/coursef-2017/lessons/8/levels/4
+
+This time, help the bee collect all of the nectar using as few blocks as possible.
+
+---
+Here are elements from the toolbox.
+You can use them in your code:
+```
+bee.forward() # limit: 1
+bee.right()
+bee.left()
+bee.get_nectar()
+for i in range(5): # limit: 2
+    # Do this
+```
+'''
+
+# When run
+
+# Start
+for i in range(3):
+    bee.forward()
+    for i in range(2):
+        bee.forward()
+        bee.get_nectar()
+    bee.right()
+
+# Keep this
+Puzzle.done()
---
+++ coursef-2017/8 - Ramp Up to Course F (Optional) - Nested Loops in Bee-Zombie/8_maze.py
@@ -0,0 +1,28 @@
+from maze import Puzzle, ZombiePlayer
+
+maze = Puzzle.from_file("courseD_maze_nestedLoops5")
+zombie: ZombiePlayer = maze.player
+
+'''
+https://studio.code.org/s/coursef-2017/lessons/8/levels/8
+
+
+
+---
+Here are elements from the toolbox.
+You can use them in your code:
+```
+zombie.forward()
+zombie.left()
+zombie.right()
+for i in range(???): # limit: 3
+    # Do this
+```
+'''
+
+# When run
+
+
+
+# Keep this
+Puzzle.done()
