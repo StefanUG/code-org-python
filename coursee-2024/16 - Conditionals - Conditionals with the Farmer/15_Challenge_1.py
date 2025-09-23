b07bacfab9b4dd52913a2a2382a61be87fcb1f42
Stefan Grinsted <stefan@e-nation.dk>
2025-09-23 17:54:45 +0200

    Added Course E 2024

---
+++ coursee-2024/16 - Conditionals - Conditionals with the Farmer/15_Challenge_1.py
@@ -0,0 +1,38 @@
+from maze import Puzzle, Harvester
+
+maze = Puzzle.from_file("courseE_farmer_ramp12f_2024")
+farmer: Harvester = maze.player
+
+'''
+https://studio.code.org/s/coursee-2024/lessons/16/levels/15
+
+**Challenge**: The only way to get all of the crops is to move forward until you get to the lettuce, then turn left **if** there is a path to the left.  Otherwise, turn right.
+
+---
+Here are elements from the toolbox.
+You can use them in your code:
+```
+farmer.forward()
+farmer.right()
+farmer.left()
+farmer.pick_lettuce()
+if farmer.path_left():
+    # Do this
+else:
+    # Otherwise this
+while farmer.has_lettuce():
+    # Do this
+while not farmer.has_lettuce():
+    # Do this
+for i in range(5):
+    # Do this
+# 
+```
+'''
+
+# When run
+
+# Start
+
+# Keep this
+Puzzle.done()
---
+++ coursee-2024/16 - Conditionals - Conditionals with the Farmer/16_Practice_1.py
@@ -0,0 +1,38 @@
+from maze import Puzzle, Harvester
+
+maze = Puzzle.from_file("courseE_farmer_while1_2024")
+farmer: Harvester = maze.player
+
+'''
+https://studio.code.org/s/coursee-2024/lessons/16/levels/16
+
+Move forward until you get to the lettuce, then turn left **if** there is a path to the left.  Otherwise, turn right.
+
+---
+Here are elements from the toolbox.
+You can use them in your code:
+```
+farmer.forward() # limit: 1
+farmer.right()
+farmer.left()
+farmer.pick_lettuce()
+if farmer.path_left():
+    # Do this
+else:
+    # Otherwise this
+while farmer.has_lettuce():
+    # Do this
+while not farmer.has_lettuce():
+    # Do this
+for i in range(5):
+    # Do this
+# 
+```
+'''
+
+# When run
+
+# Start
+
+# Keep this
+Puzzle.done()
